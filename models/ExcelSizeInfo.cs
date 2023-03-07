2a0dbb597b2c3d6d45defbffe84a0392ef52e4c7
bysxiang <bysxrokr@qq.com>
2023-03-07 22:27:21 +0800

    添加ExcelSizeInfo

---
+++ models/ExcelSizeInfo.cs
@@ -0,0 +1,44 @@
+﻿using Bysxiang.UipathExcelEx.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bysxiang.UipathExcelEx.models
+{
+    public class ExcelSizeInfo
+    {
+        public int Row { get; }
+
+        public int Column { get; }
+
+        public int RowCount { get; }
+
+        public int ColumnCount { get; }
+
+        public string ColumnName
+        {
+            get
+            {
+                return ExcelUtils.ToColumnName(Column);
+            }
+        }
+
+        public string EndColumnName
+        {
+            get
+            {
+                return ExcelUtils.ToColumnName(Row + RowCount - 1);
+            }
+        }
+
+        public string RangeStr
+        {
+            get
+            {
+                return string.Format("{0}{1}:{2}{3}", ColumnName, Row, EndColumnName, Row + RowCount - 1);
+            }
+        }
+    }
+}
