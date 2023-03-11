73c7ca0858663429b5a8db28c7eda0045b167d47
bysxiang <bysxrokr@qq.com>
2023-03-11 10:51:35 +0800

    添加LocalDisplayName属性类。

---
+++ attributes/LocalDisplayNameAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Bysxiang.UipathExcelEx.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bysxiang.UipathExcelEx.attributes
+{
+    internal class LocalDisplayNameAttribute : DisplayNameAttribute
+    {
+        public LocalDisplayNameAttribute(string displayName) : base(displayName)
+        {
+        }
+
+        public override string DisplayName => Resources.ResourceManager.GetString(this.DisplayNameValue) ?? base.DisplayName;
+    }
+}
