1e5f9ca03474e17d2838284263854db387a01fb3
bysxiang <bysxrokr@qq.com>
2023-03-13 22:10:26 +0800

    添加资源相关类。

---
+++ Properties/Excel_Activities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bysxiang.UipathExcelEx.Properties
+{
+    public static class Excel_Activities
+    {
+        private static ResourceManager rm = Resources.ResourceManager;
+
+        public static string ExcelUsedRange_Name => rm.GetString(nameof(ExcelUsedRange_Name));
+
+        public static string SizeInfo_Name => rm.GetString(nameof(SizeInfo_Name));
+
+        public static string ExcelUsedRangeException => rm.GetString(nameof(ExcelUsedRangeException));
+    }
+}
